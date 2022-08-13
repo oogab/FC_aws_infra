f47ce70d66fa688d3e2c31d384081026f0f8d41e
oogab <38171845+oogab@users.noreply.github.com>
2022-08-14 00:29:41 +0900

    add henge.link with www

--- fastcampus/settings.py
+++ fastcampus/settings.py
@@ -30,7 +30,8 @@ ALLOWED_HOSTS = ['172.31.45.2', '172.31.41.84', '172.31.36.156',
     'ec2-15-164-217-126.ap-northeast-2.compute.amazonaws.com',
     'ec2-3-35-208-7.ap-northeast-2.compute.amazonaws.com',
     'fc-aws-infra-ELB-2123070017.ap-northeast-2.elb.amazonaws.com',
-    '*.henge.link'
+    'www.henge.link',
+    'henge.link'
 ]
 
 
